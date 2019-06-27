7e31b4c82db88d05fbb2bb83b60b67bbd1b3abb4
mathletics-book <42146212+mathletics-book@users.noreply.github.com>
2019-06-27 14:33:03 -0400

    Add files via upload

--- Chapter-3/hitters_regression.py
+++ Chapter-3/hitters_regression.py
@@ -1,1 +1,25 @@
+###########################################################################
+### Chapter 3 - Evaluating Hitters by Linear Weights                    ###
+# Mathletics: How Gamblers, Managers, and Sports Enthusiasts              #
+# Use Mathematics in Baseball, Basketball, and Football                   #
+###########################################################################
 
+import pandas as pd
+import statsmodels.api as sm
+
+df = pd.read_csv("data_hitters_linear_weights.csv")
+
+# remove the columns that you do not need for the regression; i.e., 'Yr' and 'Tm'
+
+df = df.drop(['Yr', 'Tm'], axis = 1) 
+
+# create the independent variables
+X = df.loc[:, df.columns != 'R']
+# add the constant term 
+X = sm.add_constant(X)
+# create the dependent variable
+y = pd.DataFrame(df['R'])
+
+model = sm.OLS(y, X).fit()
+
+print(model.summary())
