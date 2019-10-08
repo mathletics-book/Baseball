d0e6f38a50e13d061d36f02fd36639c82f9e49b8
mathletics-book <42146212+mathletics-book@users.noreply.github.com>
2019-10-08 16:15:30 -0400

    Add files via upload

---
+++ Chapter-11/WaldWolfowitzRunsTest.py
@@ -0,0 +1,28 @@
+###########################################################################
+### Chapter 11 - Streakiness in Sports                                  ###
+# Mathletics: How Gamblers, Managers, and Sports Enthusiasts              #
+# Use Mathematics in Baseball, Basketball, and Football                   #
+###########################################################################
+
+# this is an implementation of WWRT
+
+import numpy as np
+
+
+def find_runs(seq):
+	return 1+sum([1 if seq[i]!=seq[i+1] else 0 for i in range(len(seq)-1)])
+
+def WWRT(seq):
+	s = sum(seq)
+	f = len(seq)-s
+	mu = ((2*s*f)/(s+f))+1
+	sigma = np.sqrt((mu-1)*(mu-2)/(s+f-1))
+	return (find_runs(seq)-mu)/sigma
+
+# two sequences for illustrative purposes 
+seq1 = [0,1,0,0,1,1,1,1,1,0,1,1,0,0,1,1,0,1,1,0,0,1,0,1,0]
+seq2 = [0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0]
+
+print("Sequence: ",seq1, "has a WWRT z-score of: ", WWRT(seq1))
+print("Sequence: ",seq2, "has a WWRT z-score of: ", WWRT(seq2))
+
