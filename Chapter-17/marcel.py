26adc4d09d668e910bc8300f74ffa180dbad8238
mathletics-book <42146212+mathletics-book@users.noreply.github.com>
2019-10-08 21:34:50 -0400

    Add files via upload

--- Chapter-17/marcel.py
+++ Chapter-17/marcel.py
@@ -1,1 +1,6 @@
+###########################################################################
+### Chapter 17 - Projecting Major League Performance                    ###
+# Mathletics: How Gamblers, Managers, and Sports Enthusiasts              #
+# Use Mathematics in Baseball, Basketball, and Football                   #
+###########################################################################
 
